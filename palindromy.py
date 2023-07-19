57128a2823377158002b17a9995b72bef11d77fb
Marcin <80240893+MarcinAn@users.noreply.github.com>
2023-07-19 20:34:50 +0200

    Update palindromy.py

--- palindromy.py
+++ palindromy.py
@@ -1,5 +1,5 @@
 def check_if_palindromes(word):
-    return word.lower() == word[::-1].lower()
+    return word == word[::-1]
 
 usr_word = input('Podaj słowo: ')
-print(check_if_palindromes(usr_word))
+print(check_if_palindromes(usr_word.lower()))
